964aaf234804047a1dd9e767c39103878181ded3
Oery <48154940+Oery@users.noreply.github.com>
2024-09-03 18:41:51 +0200

    Add logging

--- src-tauri/src/modules/auto_queuing_scene/controller.rs
+++ src-tauri/src/modules/auto_queuing_scene/controller.rs
@@ -1,5 +1,6 @@
 use crate::states::config::Settings;
 
+use log::error;
 use tauri::{AppHandle, Manager};
 
 use super::scene_manager::SceneState;
@@ -7,13 +8,17 @@ use super::scene_manager::SceneState;
 async fn show_screen(app: &AppHandle) {
     let state = app.state::<SceneState>().clone();
     let scene_manager = state.scene_manager.read().await;
-    let _ = scene_manager.show_screen().await;
+    if let Err(e) = scene_manager.show_screen().await {
+        error!("[OBS] Error while trying to show screen : {}", e);
+    }
 }
 
 async fn hide_screen(app: &AppHandle, scene: &str) {
     let state = app.state::<SceneState>().clone();
     let mut scene_manager = state.scene_manager.write().await;
-    let _ = scene_manager.hide_screen(scene).await;
+    if let Err(e) = scene_manager.hide_screen(scene).await {
+        error!("[OBS] Error while trying to hide screen : {}", e);
+    }
 }
 
 pub async fn set_screen(app: &AppHandle, settings: &Settings, should_show: bool) {
