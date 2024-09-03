4699f27c2623a26e94a1617f4f1f4106cd0c9a02
Oery <48154940+Oery@users.noreply.github.com>
2024-09-03 19:56:57 +0200

    Add support for OBS WS 30.0.1

--- src-tauri/src/modules/auto_queuing_scene/socket_config.rs
+++ src-tauri/src/modules/auto_queuing_scene/socket_config.rs
@@ -1,61 +1,24 @@
-use std::env;
+use anyhow::Result;
+use log::{info, warn};
+use serde::Deserialize;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-use log::{error, info};
+use crate::tailer::utils::format_path;
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct SocketConfig {
+    #[serde(rename = "server_enabled")]
     pub enabled: bool,
+    #[serde(rename = "server_port")]
     pub port: u16,
     pub auth_required: bool,
+    #[serde(rename = "server_password")]
     pub password: String,
 }
 
 impl SocketConfig {
-    pub fn fetch_socket_config(&mut self) -> io::Result<()> {
-        let app_data = match env::var("APPDATA") {
-            Ok(path) => path,
-            Err(_) => {
-                error!("[OBS] APPDATA not found");
-                return Err(io::Error::new(io::ErrorKind::NotFound, "APPDATA not found"));
-            }
-        };
-
-        let obs_config_path = Path::new(&app_data).join("obs-studio/global.ini");
-        let file = File::open(obs_config_path)?;
-
-        info!("[OBS] Loading config...");
-
-        for line in io::BufReader::new(file).lines() {
-            let line = line?;
-
-            match line.as_str() {
-                value if value.starts_with("ServerEnabled=") => {
-                    self.enabled = value.contains("true");
-                }
-                value if value.starts_with("ServerPort=") => {
-                    self.port = match value.split_once('=').unwrap().1.parse::<u16>() {
-                        Ok(value) => value,
-                        Err(_) => continue,
-                    };
-                }
-                value if value.starts_with("AuthRequired=") => {
-                    self.auth_required = value.contains("true");
-                }
-                value if value.starts_with("ServerPassword=") => {
-                    self.password = value.split_once('=').unwrap().1.to_string();
-                }
-                _ => continue,
-            }
-        }
-
-        info!("[OBS] Config loaded");
-
-        Ok(())
-    }
-
     pub fn new() -> Self {
         Self {
             enabled: false,
@@ -63,5 +26,54 @@ impl SocketConfig {
             auth_required: false,
             password: String::new(),
         }
+    }
+
+    pub fn load(path: &str) -> Result<Self> {
+        info!("[OBS] Loading config...");
+
+        let formatted_path = format_path(path);
+        let json_config_path = Path::new(&formatted_path).join("plugin_config/obs-websocket/config.json");
+
+        // Only works on obs-studio around < 30.0.1
+        // Config was migrated to a json file in plugins-config/obs-websocket/config.json
+        if json_config_path.exists() {
+            info!("[OBS] JSON config file found, loading it");
+            return Ok(serde_json::from_reader(File::open(json_config_path)?)?);
+        }
+
+        warn!("[OBS] Config file not found, trying legacy config. You should update your obs-studio version.");
+
+        let legacy_config_path = Path::new(path).join("global.ini");
+        Self::read_obs_config_file(&legacy_config_path)
+    }
+
+    pub fn read_obs_config_file(path: &std::path::PathBuf) -> Result<Self> {
+        let file = File::open(path)?;
+        let mut config = Self::new();
+
+        for line in io::BufReader::new(file).lines() {
+            let line = line?;
+
+            match line.as_str() {
+                value if value.starts_with("ServerEnabled=") => {
+                    config.enabled = value.contains("true");
+                }
+                value if value.starts_with("ServerPort=") => {
+                    config.port = match value.split_once('=').unwrap().1.parse::<u16>() {
+                        Ok(value) => value,
+                        Err(_) => continue,
+                    };
+                }
+                value if value.starts_with("AuthRequired=") => {
+                    config.auth_required = value.contains("true");
+                }
+                value if value.starts_with("ServerPassword=") => {
+                    config.password = value.split_once('=').unwrap().1.to_string();
+                }
+                _ => continue,
+            }
+        }
+
+        Ok(config)
     }
 }
