9dfe4379f694223eef3959a735eb2fc8bc1a8532
Oery <48154940+Oery@users.noreply.github.com>
2024-09-03 15:24:24 +0200

    Add support for bot features

---
+++ src/utils/bot-features.ts
@@ -0,0 +1,14 @@
+const botFeatures: Record<string, string[]> = {
+    self: ["custom_commands", "edit_commands"],
+    custom: ["custom_commands", "edit_commands"],
+    nightbot: ["custom_commands", "edit_commands"],
+    wizebot: ["edit_commands"],
+};
+
+export function getBotFeatures(bot: string): string[] {
+    return botFeatures[bot as string];
+}
+
+export function hasBotFeature(bot: string, feature: string): boolean {
+    return getBotFeatures(bot).includes(feature);
+}
